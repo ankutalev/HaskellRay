1fcdba9b1e212341d61cc5651f364541287f32c5
ankutalev <a.kutalev@g.nsu.ru>
2019-12-22 15:55:28 +0700

    tabs replaced

--- SceneParser.hs
+++ SceneParser.hs
@@ -16,39 +16,38 @@ trim = f . f
    
 parser::(FromList a)=>[String]->(Double->Bool)->Maybe a
 parser x f | any (==Nothing) readed = Nothing
-		   | otherwise = fromList $ filter f $ fromJust <$> readed 
-			where 
-				 readed::[Maybe Double]
-				 readed = readMaybe <$> x
+           | otherwise = fromList $ filter f $ fromJust <$> readed 
+            where 
+                 readed::[Maybe Double]
+                 readed = readMaybe <$> x
 
 parseColor::[String]->Maybe (Color Int)
 parseColor arg = parser arg (\x-> x>=0 && x<= 255 )
-				  
+                  
 parsePoint::[String]->Maybe (Point Double)
 parsePoint arg = parser arg (\x->True)
 
 
-								 
-parseLightPoints :: [String] -> Maybe LightPoint	
+                                 
+parseLightPoints :: [String] -> Maybe LightPoint    
 parseLightPoints arg = do
-						 let (p,c) = splitAt 3 arg
-						 p <- parsePoint p
-						 c <- parseColor c
-						 return $ LightPoint p c 
-						 
+                         let (p,c) = splitAt 3 arg
+                         p <- parsePoint p
+                         c <- parseColor c
+                         return $ LightPoint p c 
+                         
 parseObjects::String ->Maybe Scene
 parseObjects content = do
-						 let (diffuse:n:body) =  filter (\x->x /="" && not ("//" `isPrefixOf` x)) $ trim <$> lines content
-						 diffusion <- parseColor . words $ diffuse
-						 let (lp,primitives) = splitAt (read n) body
-						 lightPoints <- mapM parseLightPoints ( words <$> lp)
-						 return $ Scene diffusion lightPoints
-						 
+                         let (diffuse:n:body) =  filter (\x->x /="" && not ("//" `isPrefixOf` x)) $ trim <$> lines content
+                         diffusion <- parseColor . words $ diffuse
+                         let (lp,primitives) = splitAt (read n) body
+                         lightPoints <- mapM parseLightPoints ( words <$> lp)
+                         return $ Scene diffusion lightPoints
+                         
 parseScene::String->IO (Maybe Scene)
 parseScene fileName = withFile fileName ReadMode (\h -> do
-															content <- hGetContents h
-															let scene = parseObjects content
-															print scene
-															return scene
-												  )
-						 
+                                                          content <- hGetContents h
+                                                          let scene = parseObjects content
+                                                          print scene
+                                                          return scene
+                                                  )
